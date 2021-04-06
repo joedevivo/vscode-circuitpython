27b6cf3df8a7d6c524a6c13dafd5aa8ab6c9f00d
Joe DeVivo <joe@devivo.com>
2021-04-06 05:33:49 -0700

    Cleanup build process

--- scripts/install-bindings.js
+++ scripts/install-bindings.js
@@ -2,22 +2,25 @@
 const globby = require('globby');
 var path=require('path');
 var fs = require('fs');
-/*
-const nodeAbi = require('node-abi');
-var abi = nodeAbi.getAbi('11.3.0', 'electron');
-var dir = `node-${abi}-${process.platform}-${process.arch}`;
-console.log(` Installing bindings for ${dir}`);
-*/
-let re = new RegExp('bindings/node_modules/(.*)/lib/binding/(.*)/(.*).node', '');
+let re = new RegExp('node_modules/(.*)/lib/binding/(.*)/(.*).node', '');
 (async () => {
-    var filez = await globby('bindings/node_modules/**/lib/binding/*/*.node');
+    var filez = await globby('../bindings-*/node_modules/**/lib/binding/*/*.node');
     filez.forEach(f => {
-        console.log(f);
+        console.log(`Found ${f}`);
         dest = f.replace(re, 'node_modules/$1/lib/binding/$2/$3.node');
         parent = path.dirname(dest);
-        console.log(parent);
+        console.log(`Moving ${parent}`);
+        console.log(`    to ${dest}`);
         fs.mkdirSync(parent, {recursive: true});
-        fs.renameSync(f, dest);
-        console.log(dest);  
+        fs.renameSync(f, dest); 
+    });
+})();
+(async () => {
+    var filez = await globby('node_modules/**/build/Release/*.node');
+    filez.forEach(f => {
+        dest = f.replace(re, 'node_modules/$1/lib/binding/$2/$3.node');
+        parent = path.dirname(dest);;
+        console.log(`Deleting ${parent}`);
+        fs.rmdirSync(parent, {recursive: true});
     });
 })();
