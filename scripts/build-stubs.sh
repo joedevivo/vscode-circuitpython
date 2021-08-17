25b90cebf3f47c54508467345998550a7d147f29
Ryan A. Pavlik <ryan.pavlik@gmail.com>
2021-08-17 08:25:06 -0500

    stubmaker remove prefix (#56)
    * Improve build-stubs.sh
    
    Ensure we use the directory we expect, don't accidentally nest stub directory,
    use venv for python modules.
    
    * build_stubs.py: Normalize vid/pid case.
    
    * build_stubs.py: Handle CircuitPython 7 BLE-only boards better.
    
    * build_stubs.py: Use pathlib more fully to simplify
    
    * build_stubs.py: Add "from __future__ import annotations"
    
    * build_stubs.py: factor out the pins parsing.
    
    * build_stubs.py: Handle built-in displays better.
    
    * build_stubs.py: Misc cleanups
    
    * build_stubs.py: improve import detecting.
    
    * build_stubs.py: Refactor entirely into functions.
    
    More testable this way, etc.
    
    * build_stubs.py: Be careful not to get commented-out definitions.
    
    * build_stubs.py: Further clean up pin parsing.
    
    Also makes sure that we don't make a stub for a commented pin.
    
    * build_stubs.py: Most boards don't need Any now
    
    * build_stubs.py: Format with Black.
    
    * build_stubs.py: Improve finding only boards.
    
    * build_stubs.py: Simplify parse_generic_stub
    
    * build_stubs.py: Remove "board." and two-space indents from stubs we generate.

--- scripts/build-stubs.sh
+++ scripts/build-stubs.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
-git clone --depth 1 --branch main https://github.com/adafruit/circuitpython.git
-cd circuitpython
-git submodule init
-git submodule update extmod/ulab
-pip3 install -r docs/requirements.txt
-make stubs
-mv circuitpython-stubs ../stubs
-cd ..
-pip3 install -r requirements.txt
-python3 ./scripts/build_stubs.py
-rm -rf stubs/board
+(
+    # Current dir should be the root of the repo
+    cd $(dirname $0)/..
+
+    git clone --depth 1 --branch main https://github.com/adafruit/circuitpython.git
+
+    cd circuitpython
+    git submodule init
+    git submodule update extmod/ulab
+
+    # Use a venv for these
+    # Using this name so circuitpython repo already gitignores it
+    python3 -m venv .venv/
+    . .venv/bin/activate
+
+    # `make stubs` in circuitpython
+    pip3 install wheel  # required on debian buster for some reason
+    pip3 install -r docs/requirements.txt
+    make stubs
+    if [ -d ../stubs ]; then
+        mv circuitpython-stubs/* ../stubs/
+    else
+        # if stubs already exists, this would get interpreted as "move circuitpython-stubs *into* stubs"
+        # hence the "if".  Friendlier than the alternative, `rm -rf stubs`
+        mv circuitpython-stubs/ ../stubs
+    fi
+    cd ..
+
+    # scripts/build_stubs.py in this repo for board stubs
+    pip3 install -r requirements.txt
+    python3 ./scripts/build_stubs.py
+    rm -rf stubs/board
+
+    # get out of venv
+    deactivate
+)
--- scripts/build_stubs.py
+++ scripts/build_stubs.py
@@ -1,108 +1,189 @@
-import appdirs
-import subprocess
-import os
-#import mypy
-import pathlib
-import re
-import glob
-import json
-
+#!/usr/bin/env python3
 # This is a script for using circuitpython's repo to make pyi files for each board type.
 # These need to be bundled with the extension, which means that adding new boards is still
 # a new release of the extension.
 
-# First thing we want to do is store in memory, the contents of 
-# ./circuitpython/circuitpython-stubs/board/__init__.py so we can append it to
-# every other board.
-board_stub = pathlib.Path(os.path.join("./stubs/board", "__init__.pyi"))
+# import mypy
+import json
+import pathlib
+import re
 
-# See [Issue #26](https://github.com/joedevivo/vscode-circuitpython/issues/26) 
-# for more on this.
-generic_stubs = {}
-with open(board_stub) as stub:
-  stubs = stub.readlines()
-  i = 0
-  f = []
-  for s in stubs:
-    if s.startswith('def'):
-      f.append(i)
-    i += 1
-  f.append(i)
 
-  x = f.pop(0)
-  for y in f:
-    it = '  ' + ''.join(stubs[x:y-1])
-    r = re.search(r'def ([^\(]*)\(', it)
-    k = r[1]
-    generic_stubs[k] = it
-    x = y
+def main():
+    repo_root = pathlib.Path(__file__).resolve().parent.parent
+    # First thing we want to do is store in memory, the contents of
+    # ./stubs/board/__init__.py so we can append it (partially) to
+    # every other board.
+    # See [Issue #26](https://github.com/joedevivo/vscode-circuitpython/issues/26)
+    # for more on this.
+    board_stub = repo_root / "stubs" / "board" / "__init__.pyi"
+    generic_stubs = parse_generic_stub(board_stub)
+
+    circuitpy_repo_root = repo_root / "circuitpython"
+    boards = process_boards(repo_root, circuitpy_repo_root, generic_stubs)
+
+    json_file = repo_root / "boards" / "metadata.json"
+    with open(json_file, "w") as metadata:
+        json.dump(boards, metadata)
+
+
+def parse_generic_stub(board_stub):
+    generic_stubs = {}
+    def_re = re.compile(r"def ([^\(]*)\(.*")
+    with open(board_stub) as stub:
+        stubs = stub.readlines()
+
+        # Find the first line number and name of each definition
+        f = []
+        names = []
+        for i, s in enumerate(stubs):
+            match = def_re.match(s)
+            if match is not None:
+                f.append(i)
+                names.append(match[1])
+        f.append(len(stubs))
+
+        # Iterate the line ranges
+        for name, start, end in zip(names, f, f[1:]):
+            generic_stubs[name] = "".join(stubs[start:end])
+    return generic_stubs
+
+
+def normalize_vid_pid(vid_or_pid: str):
+    """Make a hex string all uppercase except for the 0x."""
+    return vid_or_pid.upper().replace("0X", "0x")
+
+
+_PIN_DEF_RE = re.compile(
+    r"\s*{\s*MP_ROM_QSTR\(MP_QSTR_(?P<name>[^\)]*)\)\s*,\s*MP_ROM_PTR\((?P<value>[^\)]*)\).*"
+)
+
+
+def parse_pins(generic_stubs, pins: pathlib.Path, board_stubs):
+    imports = set()
+    stub_lines = []
+    with open(pins) as p:
+        for line in p:
+            pin = _PIN_DEF_RE.match(line)
+            if pin is None:
+                continue
+            pin_name = pin.group("name")
+            if pin_name in generic_stubs:
+                board_stubs[pin_name] = generic_stubs[pin_name]
+                if "busio" in generic_stubs[pin_name]:
+                    imports.add("busio")
+                continue
+
+            pin_type = None
+
+            # sometimes we can guess better based on the value
+            pin_value = pin.group("value")
+            if pin_value.startswith("&displays"):
+                imports.add("displayio")
+                pin_type = "displayio.Display"
+            elif pin_value.startswith("&pin_"):
+                imports.add("microcontroller")
+                pin_type = "microcontroller.Pin"
+
+            if pin_type is None:
+                imports.add("typing")
+                pin_type = "typing.Any"
+
+            stub_lines.append("{0}: {1} = ...\n".format(pin_name, pin_type))
+
+    imports_string = "".join("import %s\n" % x for x in sorted(imports))
+
+    stubs_string = "".join(stub_lines)
+    return imports_string, stubs_string
+
 
 # now, while we build the actual board stubs, replace any line that starts with `  $name:` with value
 
-board_dirs = glob.glob("circuitpython/ports/*/boards/*")
-boards = []
-for b in board_dirs :
-  site_path = os.path.split(b)[-1]
 
-  config = pathlib.Path(os.path.join(b, "mpconfigboard.mk"))
-  print(config)
-  pins   = pathlib.Path(os.path.join(b, "pins.c"))
-  if config.is_file() and pins.is_file():
+def process_boards(repo_root, circuitpy_repo_root, generic_stubs):
+    boards = []
 
-    usb_vid = ""
-    usb_pid = ""
-    usb_product = ""
-    usb_manufacturer = ""
-    with open(config) as conf:
-      for line in conf:
-        if line.startswith("USB_VID"):
-          usb_vid = line.split("=")[1].split("#")[0].strip('" \n')
-        elif line.startswith("USB_PID"):
-          usb_pid = line.split("=")[1].split("#")[0].strip('" \n')
-        elif line.startswith("USB_PRODUCT"):
-          usb_product = line.split("=")[1].split("#")[0].strip('" \n')
-        elif line.startswith("USB_MANUFACTURER"):
-          usb_manufacturer = line.split("=")[1].split("#")[0].strip('" \n')
-    if usb_manufacturer == "Nadda-Reel Company LLC":
-      continue
+    board_configs = circuitpy_repo_root.glob("ports/*/boards/*/mpconfigboard.mk")
+    for config in board_configs:
+        b = config.parent
+        site_path = b.stem
 
-    board = { 'vid': usb_vid, 'pid': usb_pid, 'product': usb_product, 'manufacturer': usb_manufacturer, 'site_path': site_path }
-    boards.append(board)
-    print("{0}:{1} {2}, {3}".format(usb_vid, usb_pid, usb_manufacturer, usb_product))
-    board_pyi_path = pathlib.Path(os.path.join("boards", usb_vid, usb_pid))
-    board_pyi_path.mkdir(parents=True, exist_ok=True)
-    board_pyi_file = pathlib.Path(os.path.join(board_pyi_path, "board.pyi"))
+        print(config)
+        pins = b / "pins.c"
+        if not config.is_file() or not pins.is_file():
+            continue
 
-    # Indent 0 char for the first pin, 2 for the rest
-    indent = ""
+        usb_vid = ""
+        usb_pid = ""
+        usb_product = ""
+        usb_manufacturer = ""
+        with open(config) as conf:
+            for line in conf:
+                if line.startswith("USB_VID"):
+                    usb_vid = line.split("=")[1].split("#")[0].strip('" \n')
+                elif line.startswith("USB_PID"):
+                    usb_pid = line.split("=")[1].split("#")[0].strip('" \n')
+                elif line.startswith("USB_PRODUCT"):
+                    usb_product = line.split("=")[1].split("#")[0].strip('" \n')
+                elif line.startswith("USB_MANUFACTURER"):
+                    usb_manufacturer = line.split("=")[1].split("#")[0].strip('" \n')
 
-    # We're going to put the common stuff from the generic board stub at the 
-    # end of the file, so we'll collect them after the loop
-    board_stubs = {}
+                # CircuitPython 7 BLE-only boards
+                elif line.startswith("CIRCUITPY_CREATOR_ID"):
+                    usb_vid = line.split("=")[1].split("#")[0].strip('" \n')
+                elif line.startswith("CIRCUITPY_CREATION_ID"):
+                    usb_pid = line.split("=")[1].split("#")[0].strip('" \n')
+        if usb_manufacturer == "Nadda-Reel Company LLC":
+            continue
 
-    with open(board_pyi_file, 'w') as outfile, open(pins) as p:
-      outfile.write("from typing import Any\n")
-      outfile.write('"""\n')
-      outfile.write('board {0} {1}\n'.format(board['manufacturer'], board['product']))
-      outfile.write('https://circuitpython.org/boards/{0}\n'.format(board['site_path']))
-      outfile.write('"""\n')
-      outfile.write("  board.")
-      for line in p:
-        pin = re.search(r'.*_QSTR\(MP_QSTR_([^\)]*)', line)
-        if pin == None: 
-          continue
-        pin_name = pin[1]
-        if pin_name in generic_stubs:
-          board_stubs[pin_name] = generic_stubs[pin_name]
-          continue
-        else:
-          outfile.write("{0}{1}: Any = ...\n".format(indent, pin_name))
-          #redefine indent every time
-          indent = "  "
-      # End for
-      for p in board_stubs:
-        outfile.write("{0}\n".format(board_stubs[p]))
-    
-json_file = pathlib.Path(os.path.join("boards", "metadata.json"))
-with open(json_file, 'w') as metadata:
-  json.dump(boards, metadata)
+        usb_vid = normalize_vid_pid(usb_vid)
+        usb_pid = normalize_vid_pid(usb_pid)
+
+        # CircuitPython 7 BLE-only boards have no usb manuf/product
+        description = site_path
+        if usb_manufacturer and usb_product:
+            description = "{0} {1}".format(usb_manufacturer, usb_product)
+
+        board = {
+            "vid": usb_vid,
+            "pid": usb_pid,
+            "product": usb_product,
+            "manufacturer": usb_manufacturer,
+            "site_path": site_path,
+            "description": description,
+        }
+        boards.append(board)
+        print(
+            "{0}:{1} {2}, {3}".format(usb_vid, usb_pid, usb_manufacturer, usb_product)
+        )
+        board_pyi_path = repo_root / "boards" / usb_vid / usb_pid
+        board_pyi_path.mkdir(parents=True, exist_ok=True)
+        board_pyi_file = board_pyi_path / "board.pyi"
+
+        # We're going to put the common stuff from the generic board stub at the
+        # end of the file, so we'll collect them after the loop
+        board_stubs = {}
+
+        with open(board_pyi_file, "w") as outfile:
+            imports_string, stubs_string = parse_pins(generic_stubs, pins, board_stubs)
+            outfile.write("from __future__ import annotations\n")
+            outfile.write(imports_string)
+
+            # start of module doc comment
+            outfile.write('"""\n')
+            outfile.write("board {0}\n".format(board["description"]))
+            outfile.write(
+                "https://circuitpython.org/boards/{0}\n".format(board["site_path"])
+            )
+            outfile.write('"""\n')
+
+            # start of actual stubs
+            outfile.write(stubs_string)
+
+            for p in board_stubs:
+                outfile.write("{0}\n".format(board_stubs[p]))
+    return boards
+
+
+if __name__ == "__main__":
+    main()
