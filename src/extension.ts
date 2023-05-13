4dbccdebd256eefd18aebab27ec4df3071fcf9bd
Justin Wagner <jrwagz@gmail.com>
2023-05-13 14:00:46 -0600

    Mimic syntax in other parts of repo

--- src/extension.ts
+++ src/extension.ts
@@ -5,10 +5,9 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.getConfiguration().update("python.languageServer", "Pylance");
 	vscode.workspace.getConfiguration().update("python.linting.pylintEnabled", false);
 
-	let config_key = "python.analysis.diagnosticSeverityOverrides"
-	let config_value = vscode.workspace.getConfiguration()[config_key];
-	config_value["reportMissingModuleSource"] = "none";
-	vscode.workspace.getConfiguration().update(config_key,config_value);
+	let config_key: string = "python.analysis.diagnosticSeverityOverrides"
+	let config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration(config_key);
+	config.update("reportMissingModuleSource", "none");
 	let container: Container = await Container.newInstance(context);
 }
 
