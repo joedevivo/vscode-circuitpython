79bb682196700a575726bf8c2ce1f5ca4f00325e
Justin Wagner <jrwagz@gmail.com>
2023-05-13 13:54:28 -0600

    Improved variable names

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,9 @@ export async function activate(context: vscode.ExtensionContext) {
 	vscode.workspace.getConfiguration().update("python.linting.pylintEnabled", false);
 
 	let config_key = "python.analysis.diagnosticSeverityOverrides"
-	let current_value = vscode.workspace.getConfiguration()[config_key];
-	current_value["reportMissingModuleSource"] = "none";
-	vscode.workspace.getConfiguration().update(config_key,current_value);
+	let config_value = vscode.workspace.getConfiguration()[config_key];
+	config_value["reportMissingModuleSource"] = "none";
+	vscode.workspace.getConfiguration().update(config_key,config_value);
 	let container: Container = await Container.newInstance(context);
 }
 
