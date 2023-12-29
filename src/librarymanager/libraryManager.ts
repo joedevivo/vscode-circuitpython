df8e21983467a123e7472957628a6d57fe6d1a4f
Joe DeVivo <joe@devivo.com>
2023-12-29 10:40:43 -0700

    Update bundle for 8.x, 9.x

--- src/librarymanager/libraryManager.ts
+++ src/librarymanager/libraryManager.ts
@@ -1,15 +1,15 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
-import * as axios from 'axios';
-import * as unzip from 'unzipper';
-import { String } from 'typescript-string-operations';
-import * as _ from 'lodash';
-import { Library } from './library';
-import * as globby from 'globby';
-import * as fs_extra from 'fs-extra';
-import * as trash from 'trash';
-import { Container } from '../container';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import * as axios from "axios";
+import * as unzip from "unzipper";
+import { String } from "typescript-string-operations";
+import * as _ from "lodash";
+import { Library } from "./library";
+import * as globby from "globby";
+import * as fs_extra from "fs-extra";
+import * as trash from "trash";
+import { Container } from "../container";
 
 class LibraryQP implements vscode.QuickPickItem {
   // QuickPickItem impl
@@ -23,9 +23,9 @@ class LibraryQP implements vscode.QuickPickItem {
   public constructor(b: Library, p: Library) {
     this.bundleLib = b;
     this.projectLib = p;
-    if(b === undefined) {
+    if (b === undefined) {
       this.op = "custom";
-      if(p === null) {
+      if (p === null) {
         this.label = "Custom";
         this.description = "Cannot update";
       } else {
@@ -34,10 +34,10 @@ class LibraryQP implements vscode.QuickPickItem {
       }
     } else {
       this.label = b.name;
-      if(p === null) {
+      if (p === null) {
         this.op = "install";
         this.description = `Install version ${b.version}`;
-      } else if(b.version !== p.version) {
+      } else if (b.version !== p.version) {
         this.op = "update";
         this.description = `Update from v${p.version} to v${b.version}`;
       } else {
@@ -62,17 +62,25 @@ class LibraryQP implements vscode.QuickPickItem {
     Container.reloadProjectLibraries();
   }
   private install() {
-    let src: string = LibraryManager.getMpy(path.basename(this.bundleLib.location));
-    if(this.bundleLib.isDirectory) {
+    let src: string = LibraryManager.getMpy(
+      path.basename(this.bundleLib.location)
+    );
+    if (this.bundleLib.isDirectory) {
       fs_extra.copySync(
         src,
-        path.join(Container.getProjectLibDir(), path.basename(this.bundleLib.location)),
+        path.join(
+          Container.getProjectLibDir(),
+          path.basename(this.bundleLib.location)
+        ),
         { overwrite: true }
       );
     } else {
       fs.copyFileSync(
         src,
-        path.join(Container.getProjectLibDir(), path.basename(this.bundleLib.location, ".py") + ".mpy"),
+        path.join(
+          Container.getProjectLibDir(),
+          path.basename(this.bundleLib.location, ".py") + ".mpy"
+        )
       );
     }
   }
@@ -82,11 +90,10 @@ class LibraryQP implements vscode.QuickPickItem {
   }
 }
 export class LibraryManager implements vscode.Disposable {
-  public static BUNDLE_URL: string = "https://github.com/adafruit/Adafruit_CircuitPython_Bundle";
+  public static BUNDLE_URL: string =
+    "https://github.com/adafruit/Adafruit_CircuitPython_Bundle";
 
-  public static BUNDLE_SUFFIXES: string[] = [
-    'py', '7.x-mpy', '8.x-mpy'
-  ];
+  public static BUNDLE_SUFFIXES: string[] = ["py", "8.x-mpy", "9.x-mpy"];
   public static BUNDLE_VERSION_REGEX: RegExp = /\d\d\d\d\d\d\d\d/;
   // storageRootDir is passed in from the extension BoardManager as
   // `BoardManager.globalStoragePath` We'll keep up to date libraries here, and all
@@ -135,19 +142,21 @@ export class LibraryManager implements vscode.Disposable {
 
     // Get their metadata
     console.log(this.projectLibDir);
-    this.workspaceLibraries = await this.loadLibraryMetadata(this.projectLibDir);
+    this.workspaceLibraries = await this.loadLibraryMetadata(
+      this.projectLibDir
+    );
 
     this.cpVersion = this.getProjectCPVer();
-    if(this.cpVersion){
+    if (this.cpVersion) {
       let v: string[] = this.cpVersion.split(".");
-      if(LibraryManager.BUNDLE_SUFFIXES.includes(`${v[0]}.x-mpy`)) {
+      if (LibraryManager.BUNDLE_SUFFIXES.includes(`${v[0]}.x-mpy`)) {
         this.mpySuffix = `${v[0]}.x-mpy`;
       }
     }
   }
 
   public completionPath(): string {
-    if(this.localBundleDir === null) {
+    if (this.localBundleDir === null) {
       // In case nothing exists yet.
       return null;
     }
@@ -155,7 +164,9 @@ export class LibraryManager implements vscode.Disposable {
   }
 
   public async reloadProjectLibraries() {
-    this.workspaceLibraries = await this.loadLibraryMetadata(this.projectLibDir);
+    this.workspaceLibraries = await this.loadLibraryMetadata(
+      this.projectLibDir
+    );
   }
 
   public async show() {
@@ -189,56 +200,54 @@ export class LibraryManager implements vscode.Disposable {
 
   private getInstalledChoices(): LibraryQP[] {
     let choices: LibraryQP[] = new Array<LibraryQP>();
-    Array.from(this.workspaceLibraries.keys()).sort().forEach((v,i,a) => {
-      let b: Library = this.libraries.get(v);
-      let p: Library = this.workspaceLibraries.get(v);
-      choices.push(new LibraryQP(b, p));
-    });
+    Array.from(this.workspaceLibraries.keys())
+      .sort()
+      .forEach((v, i, a) => {
+        let b: Library = this.libraries.get(v);
+        let p: Library = this.workspaceLibraries.get(v);
+        choices.push(new LibraryQP(b, p));
+      });
     return choices;
   }
 
   private getUninstalledChoices(): LibraryQP[] {
     let choices: LibraryQP[] = new Array<LibraryQP>();
-    Array.from(this.libraries.keys()).sort().forEach((v,i,a) => {
-      let b: Library = this.libraries.get(v);
-      if (!this.workspaceLibraries.has(v)) {
-        choices.push(new LibraryQP(b, null));
-      }
-    });
+    Array.from(this.libraries.keys())
+      .sort()
+      .forEach((v, i, a) => {
+        let b: Library = this.libraries.get(v);
+        if (!this.workspaceLibraries.has(v)) {
+          choices.push(new LibraryQP(b, null));
+        }
+      });
     return choices;
   }
 
   private getProjectRoot(): string {
-    let root:string = null;
+    let root: string = null;
     vscode.workspace.workspaceFolders.forEach((f) => {
-      let r: string = path.join(
-        f.uri.fsPath
-      );
-      if(!root && fs.existsSync(r)){
-        let b: string = path.join(
-          r, "boot_out.txt"
-        );
-        if(fs.existsSync(b)) {
+      let r: string = path.join(f.uri.fsPath);
+      if (!root && fs.existsSync(r)) {
+        let b: string = path.join(r, "boot_out.txt");
+        if (fs.existsSync(b)) {
           root = r;
         }
       }
     });
-    if(!root) {
+    if (!root) {
       root = vscode.workspace.workspaceFolders[0].uri.fsPath;
     }
     return root;
   }
   // Find it boot_out, so put boot_out.txt in your project root if you want this.
   private getProjectCPVer(): string {
-    let confVer: string =
-      vscode.workspace.getConfiguration("circuitpython.board").get("version");
+    let confVer: string = vscode.workspace
+      .getConfiguration("circuitpython.board")
+      .get("version");
 
     let bootOut: string = null;
     let ver: string = null;
-    let b: string = path.join(
-      this.getProjectRoot(),
-      "boot_out.txt"
-    );
+    let b: string = path.join(this.getProjectRoot(), "boot_out.txt");
 
     let exists: boolean = fs.existsSync(b);
     // If no boot_out.txt && configured version, use configured
@@ -257,15 +266,14 @@ export class LibraryManager implements vscode.Disposable {
         ver = "unknown";
       }
     }
-    vscode.workspace.getConfiguration("circuitpython.board").update("version", ver);
+    vscode.workspace
+      .getConfiguration("circuitpython.board")
+      .update("version", ver);
     return ver;
   }
 
   private getProjectLibDir(): string {
-    let libDir: string = path.join(
-      this.getProjectRoot(),
-      "lib"
-    );
+    let libDir: string = path.join(this.getProjectRoot(), "lib");
     if (!fs.existsSync(libDir)) {
       fs.mkdirSync(libDir);
     }
@@ -275,9 +283,9 @@ export class LibraryManager implements vscode.Disposable {
   private setStorageRoot(root: string) {
     this.storageRootDir = root;
     this.bundleDir = path.join(this.storageRootDir, "bundle");
-    fs.mkdirSync(this.bundleDir, {recursive: true});
+    fs.mkdirSync(this.bundleDir, { recursive: true });
     let tag: string = this.getMostRecentBundleOnDisk();
-    if(tag !== undefined && this.verifyBundle(tag)) {
+    if (tag !== undefined && this.verifyBundle(tag)) {
       this.tag = tag;
       this.localBundleDir = path.join(this.bundleDir, tag);
     }
@@ -287,7 +295,9 @@ export class LibraryManager implements vscode.Disposable {
     let tag: string = await this.getLatestBundleTag();
     let localBundleDir: string = path.join(this.bundleDir, tag);
     if (tag === this.tag) {
-      vscode.window.showInformationMessage(`Bundle already at latest version: ${tag}`);
+      vscode.window.showInformationMessage(
+        `Bundle already at latest version: ${tag}`
+      );
     } else {
       vscode.window.showInformationMessage(`Downloading new bundle: ${tag}`);
       await this.getBundle(tag);
@@ -301,25 +311,29 @@ export class LibraryManager implements vscode.Disposable {
 
   private verifyBundle(tag: string): boolean {
     let localBundleDir: string = path.join(this.bundleDir, tag);
-    if(!fs.existsSync(localBundleDir)) {
+    if (!fs.existsSync(localBundleDir)) {
       return false;
     }
-    let bundles: fs.Dirent[] = fs.readdirSync(localBundleDir, {withFileTypes: true}).sort();
+    let bundles: fs.Dirent[] = fs
+      .readdirSync(localBundleDir, { withFileTypes: true })
+      .sort();
 
-    let suffixRegExp: RegExp = new RegExp(`adafruit-circuitpython-bundle-(.*)-${tag}`);
+    let suffixRegExp: RegExp = new RegExp(
+      `adafruit-circuitpython-bundle-(.*)-${tag}`
+    );
 
     let suffixes: string[] = [];
 
-    bundles.forEach(b => {
+    bundles.forEach((b) => {
       /*
       It's possible for some operating systems to leave files in the bundle
       directory *cough* .DS_Store *cough*. Regardless, if there's a file in
       here, we can't dig deeper in its directory tree, so we'll catch them all.
       */
-      if(b.isDirectory()) {
+      if (b.isDirectory()) {
         let p: string = path.join(localBundleDir, b.name);
-        let lib: string[] = fs.readdirSync(p).filter((v,i,a) => v === "lib");
-        if(lib.length !== 1) {
+        let lib: string[] = fs.readdirSync(p).filter((v, i, a) => v === "lib");
+        if (lib.length !== 1) {
           return false;
         }
         suffixes.push(b.name.match(suffixRegExp)[1]);
@@ -333,9 +347,9 @@ export class LibraryManager implements vscode.Disposable {
 
     // We're done. New bundle in $tag, so let's delete the ones that aren't
     // this.
-    fs.readdir(this.bundleDir, {withFileTypes: true}, (err, bundles) => {
-      bundles.forEach(b => {
-        if(b.isDirectory() && b.name !== this.tag) {
+    fs.readdir(this.bundleDir, { withFileTypes: true }, (err, bundles) => {
+      bundles.forEach((b) => {
+        if (b.isDirectory() && b.name !== this.tag) {
           let old: string = path.join(this.bundleDir, b.name);
           trash(old).then(() => null);
         }
@@ -346,26 +360,27 @@ export class LibraryManager implements vscode.Disposable {
   }
 
   private getMostRecentBundleOnDisk(): string {
-    if(!fs.existsSync(this.bundleDir)) {
+    if (!fs.existsSync(this.bundleDir)) {
       return null;
     }
-    let tag: string =
-      fs.readdirSync(this.bundleDir)
-      .filter((dir: string, i: number, a: string[]) => LibraryManager.BUNDLE_VERSION_REGEX.test(dir))
+    let tag: string = fs
+      .readdirSync(this.bundleDir)
+      .filter((dir: string, i: number, a: string[]) =>
+        LibraryManager.BUNDLE_VERSION_REGEX.test(dir)
+      )
       .sort()
       .reverse()
       .shift();
-    return(tag);
+    return tag;
   }
   /*
   Gets latest tag
   */
   private async getLatestBundleTag(): Promise<string> {
-    let r: axios.AxiosResponse =
-      await axios.default.get(
-        'https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/latest',
-        { headers : { 'Accept': 'application/json'}}
-      );
+    let r: axios.AxiosResponse = await axios.default.get(
+      "https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/latest",
+      { headers: { Accept: "application/json" } }
+    );
     return await r.data.tag_name;
   }
 
@@ -373,34 +388,36 @@ export class LibraryManager implements vscode.Disposable {
   Downloads 6.x. and source bundles. Source are crucial for autocomplete
   */
   private async getBundle(tag: string) {
-    let urlRoot: string = LibraryManager.BUNDLE_URL + '/releases/download/{0}/adafruit-circuitpython-bundle-{1}-{0}.zip';
+    let urlRoot: string =
+      LibraryManager.BUNDLE_URL +
+      "/releases/download/{0}/adafruit-circuitpython-bundle-{1}-{0}.zip";
     this.tag = tag;
     for await (const suffix of LibraryManager.BUNDLE_SUFFIXES) {
       let url: string = String.Format(urlRoot, tag, suffix);
       let p: string = path.join(this.storageRootDir, "bundle", tag);
 
-      await axios.default.get(url, {responseType: 'stream'}).then((response) => {
-        response.data.pipe(
-          unzip.Extract({path: p})
-        ).on('close', (entry) => {
-          if(suffix === 'py') {
-            Container.loadBundleMetadata();
-          };
+      await axios.default
+        .get(url, { responseType: "stream" })
+        .then((response) => {
+          response.data
+            .pipe(unzip.Extract({ path: p }))
+            .on("close", (entry) => {
+              if (suffix === "py") {
+                Container.loadBundleMetadata();
+              }
+            });
+        })
+        .catch((error) => {
+          console.log(`Error downloading {suffix} bundle: ${url}`);
         });
-      }).catch((error) => {
-        console.log(`Error downloading {suffix} bundle: ${url}`);
-      });
-    };
+    }
   }
 
   public static getMpy(name: string): string {
-    if(path.extname(name) === ".py" && Container.getMpySuffix() !== "py") {
+    if (path.extname(name) === ".py" && Container.getMpySuffix() !== "py") {
       name = path.basename(name, ".py") + ".mpy";
     }
-    return path.join(
-      Container.getBundlePath(),
-      name
-    );
+    return path.join(Container.getBundlePath(), name);
   }
 
   public bundlePath(suffix: string): string {
@@ -417,15 +434,20 @@ export class LibraryManager implements vscode.Disposable {
     return true;
   }
 
-  private async loadLibraryMetadata(rootDir: string): Promise<Map<string, Library>> {
+  private async loadLibraryMetadata(
+    rootDir: string
+  ): Promise<Map<string, Library>> {
     console.log(rootDir);
-    let libDirs: string[] =
-      await globby( '*',
-                    {absolute: true, cwd: rootDir, deep: 1, onlyFiles: false}
-      );
+    let libDirs: string[] = await globby("*", {
+      absolute: true,
+      cwd: rootDir,
+      deep: 1,
+      onlyFiles: false,
+    });
 
-    let libraries: Array<Promise<Library>> =
-      libDirs.map((p, i, a) => Library.from(p));
+    let libraries: Array<Promise<Library>> = libDirs.map((p, i, a) =>
+      Library.from(p)
+    );
 
     return new Promise<Map<string, Library>>(async (resolve, reject) => {
       let libs: Array<Library> = await Promise.all(libraries).catch((error) => {
